f7c4c3e62ce38847733588774ba3332bd9570ce1
yama <witaquanomotcamera4@gmail.com>
2026-07-21 02:00:58 +0200

    DROP

--- device.mk
+++ device.mk
@@ -28,11 +28,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service \
     vendor_kernel_prebuilts
 
-# Boot control
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    bootctrl.holi.recovery
-
 PRODUCT_PACKAGES += \
     bootctrl.holi
 
